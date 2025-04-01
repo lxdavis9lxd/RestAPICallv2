fdac8df14c05c6752bc2d24607e6224b16f72544
lxdavis9lxd <lxdavis9lxd@gmail.com>
2025-04-01 03:16:00 +0000

    committed

--- routes/restapiprocess.js
+++ routes/restapiprocess.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { callAnyRest } = require('../Modules_users/callanyrestv2'); // Ensure this path is correct
 
-router.get('restapiprocess', (req, res) => {
+router.get('/restapiprocess', (req, res) => {
     console.log('GET request received');
     res.render('restapiprocess', { title: 'REST API Processx' });
 });
 
-router.post('restapiprocess', async (req, res) => {
+router.post('/restapiprocess', async (req, res) => {
+    console.log('POST request received');
     const { url, method, body, authorization } = req.body;
-
+     console.log('Request Body:', req.body); // Log the request body for debugging
     try {
         const response = await callAnyRest(url, method, body, authorization);
         res.json(response);
